798fe306e541acfd172e9647918b013608cb5b17
Vladislav <introwlad@gamil.com>
2022-03-13 12:55:01 +0300

    refactor: fixed push on heroku

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import "dotenv/config";
 const PORT = process.env.PORT || 5000;
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+app.use(express.json())
 app.use(cors({ origin: "*", credentials: true }));
 
 // const localSession = new LocalSession()
